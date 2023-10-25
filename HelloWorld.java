b80b263ccaf52e1018b762e50bf402ae6ad6ffe8
jlowe093 <jade@jadelowe.com>
2023-10-25 17:38:34 -0400

    added name and age
    added name and age

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,14 @@ public class HelloWorld {
    public static void main (String[] args){
    
       System.out.println("Hello World!");
-   }
-}
+      
+      String name = "Jade Lowe";
+      
+      System.out.println(name);
+      
+      int age = 30;
+      
+      System.out.println(age);
+      
+   } //end main
+} // end class
